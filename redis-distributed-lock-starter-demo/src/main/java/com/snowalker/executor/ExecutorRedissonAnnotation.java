2ad1fed36cbb115c72dc3d28664702977ff6d2fa
TaXueWWL <1210812591@qq.com>
2018-12-10 09:14:48 +0800

    snowalker--在finally中释放锁

--- redis-distributed-lock-starter-demo/src/main/java/com/snowalker/executor/ExecutorRedissonAnnotation.java
+++ redis-distributed-lock-starter-demo/src/main/java/com/snowalker/executor/ExecutorRedissonAnnotation.java
@@ -1,27 +1,27 @@
-package com.snowalker.executor;
-
-import com.snowalker.lock.redisson.annotation.DistributedLock;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-/**
- * @author wuwl@19pay.com.cn
- * @date 2018-7-9
- * @desc 纯java调用
- */
-@Component
-public class ExecutorRedissonAnnotation {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorRedissonAnnotation.class);
-
-    @Scheduled(cron = "${redis.lock.cron}")
-    @DistributedLock(value = "redis-lock", expireSeconds = 11)
-    public void execute() throws InterruptedException {
-        LOGGER.info("[ExecutorRedisson]--执行定时任务开始，休眠三秒");
-        Thread.sleep(3000);
-        System.out.println("=======================业务逻辑=============================");
-        LOGGER.info("[ExecutorRedisson]--执行定时任务结束，休眠三秒");
-    }
-}
+//package com.snowalker.executor;
+//
+//import com.snowalker.lock.redisson.annotation.DistributedLock;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * @author wuwl@19pay.com.cn
+// * @date 2018-7-9
+// * @desc 纯java调用
+// */
+//@Component
+//public class ExecutorRedissonAnnotation {
+//
+//    private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorRedissonAnnotation.class);
+//
+//    @Scheduled(cron = "${redis.lock.cron}")
+//    @DistributedLock(value = "redis-lock", expireSeconds = 11)
+//    public void execute() throws InterruptedException {
+//        LOGGER.info("[ExecutorRedisson]--执行定时任务开始，休眠三秒");
+//        Thread.sleep(3000);
+//        System.out.println("=======================业务逻辑=============================");
+//        LOGGER.info("[ExecutorRedisson]--执行定时任务结束，休眠三秒");
+//    }
+//}
